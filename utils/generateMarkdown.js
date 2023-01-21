f1993375fa3a72a0f13cf89a0ba838e6faed5cd9
khamliche-jnAvzu <khamliche@gmail.com>
2023-01-21 17:30:14 +0000

    add addmarkup code

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,6 +1,37 @@
 // function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
+  ## Description
+  ${data.description}
+  
+  ## Table of Contents
+  * [Installation](#installation)
+  * [Usage](#usage)
+  * [License](#license)
+  * [Contributing](#contributing)
+  * [Tests](#tests)
+  * [Questions](#questions)
+  
+  ## Installation
+  ${data.installation}
+  
+  ## Usage
+  ${data.usage}
+  
+  ## License
+  ${data.license}
+  
+  ## Contributing
+  ${data.contributing}
+  
+  ## Tests
+  ${data.tests}
+  
+  ## Questions
+  If you have any questions, please contact me at ${data.email} or visit my GitHub profile at https://github.com/${data.github}.
+
+
+  
 
 `;
 }
